1150074986a61a7b5f26451e9f4a8603062dddf7
Tanya N <naumovets.ts@gmail.com>
2021-07-05 18:21:34 +0300

    Program activity_drill_detail and DrillDetailActivity

--- app/src/main/java/com/tata/toolsshop/DrillDetailActivity.java
+++ app/src/main/java/com/tata/toolsshop/DrillDetailActivity.java
@@ -1,14 +1,46 @@
 package com.tata.toolsshop;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DrillDetailActivity extends AppCompatActivity {
+
+    private TextView textViewTitle;
+    private TextView textViewInfo;
+    private ImageView imageViewDrill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drill_detail);
+        
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        textViewTitle = findViewById(R.id.textViewTitle);
+        textViewInfo = findViewById(R.id.textViewInfo);
+        imageViewDrill = findViewById(R.id.imageViewDrill);
+
+        Intent intent = getIntent();
+
+        if (intent.hasExtra("title") && intent.hasExtra("info") && intent.hasExtra("resId")) {
+            String title = intent.getStringExtra("title");
+            String info = intent.getStringExtra("info");
+            int resId = intent.getIntExtra("resId", -1);
+            textViewTitle.setText(title);
+            textViewInfo.setText(info);
+            imageViewDrill.setImageResource(resId);
+
+        } else {
+            Intent backToCategory = new Intent();
+            startActivity(backToCategory);
+        }
     }
 }
